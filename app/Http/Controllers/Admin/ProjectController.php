554aa7ffa2500b47f07270f41d7d933678ff6c40
Lorenzo Castiglion <lorenzo.castiglion@hotmail.it>
2023-01-11 18:28:45 +0100

    edit and delete

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -96,8 +96,10 @@ class ProjectController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Project $project)
     {
-        
+        $project->delete();
+        return redirect()->route('admin.projects.index');
+        // ->with('message', "$project->name deleted successfully");
     }
 }
--- resources/views/admin/projects/edit.blade.php
+++ resources/views/admin/projects/edit.blade.php
@@ -29,7 +29,47 @@
                         <label for="descritpion" class="form-label">Descritpion</label>
                         <textarea class="form-control" id="descritpion" name="descritpion">{{old('descritpion', $project->description)}}</textarea>
                       </div>
-                      
+
+                      <div class="mb-3">
+                        <label for="dev_lang" class="form-label">Used Dev Languages</label>
+                        <input type="text" class="form-control @error('dev_lang') is-invalid @enderror" id="dev_lang" name="dev_lang" value="{{old('dev_lang', $project->dev_lang)}}">
+                        @error('dev_lang')
+                        <div class="invalid-feedback">{{ $message }}</div>
+                        @enderror
+                      </div>
+
+                      <div class="mb-3">
+                        <label for="framework" class="form-label">Framework</label>
+                        <input type="text" class="form-control @error('framework') is-invalid @enderror" id="framework" name="framework" value="{{old('framework', $project->framework)}}">
+                        @error('framework')
+                        <div class="invalid-feedback">{{ $message }}</div>
+                        @enderror
+                      </div>
+
+                      <div class="mb-3">
+                        <label for="team" class="form-label">Team</label>
+                        <input type="text" class="form-control @error('team') is-invalid @enderror" id="team" name="team" value="{{old('team"', $project->team)}}">
+                        @error('team')
+                        <div class="invalid-feedback">{{ $message }}</div>
+                        @enderror
+                      </div>
+
+                      <div class="mb-3">
+                        <label for="git_link" class="form-label">GitHub Link</label>
+                        <input type="text" class="form-control @error('git_link') is-invalid @enderror" id="git_link" name="git_link" value="{{old('git_link', $project->git_link)}}">
+                        @error('git_link')
+                        <div class="invalid-feedback">{{ $message }}</div>
+                        @enderror
+                      </div>
+
+                      <div class="mb-3">
+                        <label for="diff_lvl" class="form-label">Difficolta</label>
+                        <input type="number" class="form-control @error('diff_lvl') is-invalid @enderror" id="diff_lvl" name="diff_lvl" value="{{old('diff_lvl', $project->diff_lvl)}}">
+                        @error('diff_lvl')
+                        <div class="invalid-feedback">{{ $message }}</div>
+                        @enderror
+                      </div>
+
                       {{-- <div class="d-flex">
                         <div class="media me-4">
                             <img class="shadow" width="150" src="{{asset('storage/' . $project->cover_image)}}" alt="{{$project->name}}">
