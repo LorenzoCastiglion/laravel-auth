aa77b30499688a8b825a4c625fd0d3de1a2c327c
Lorenzo Castiglion <lorenzo.castiglion@hotmail.it>
2023-01-11 21:44:15 +0100

    scroll su tabella index

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -8,38 +8,48 @@
             {{ session()->get('message') }}
         </div>
     @endif
-    <table class="table table-striped">
-        <thead>
-            <tr class="">
-                <th scope="col">#</th>
-                <th scope="col">Title</th>
-                <th scope="col">Description</th>
-                <th scope="col">Difficulty</th>
-                <th scope="col">Edit</th>
-                <th scope="col">Delete</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($projects as $project)
-                <tr>
-                    <th scope="row">{{ $project->id }}</th>
-                    <td><a href="{{ route('admin.projects.show', $project->slug) }}"
-                            title="View project">{{ $project->name }}</a></td>
-                    <td>{{ Str::limit($project->description, 100) }}</td>
-                    <td class="text-center">{{$project->diff_lvl}}</td>
-                    <td class="text-center"><a class="link-secondary" href="{{ route('admin.projects.edit', $project->slug) }}"
-                            title="Edit project"><i class="fa-solid fa-pen"></i></a></td>
-                    <td >
-                        <form action="{{ route('admin.projects.destroy', $project->slug) }}" method="POST">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" class="delete-button btn btn-danger ms-3 "
-                                data-item-title="{{ $project->name }}"><i class="fa-solid fa-trash-can"></i></button>
-                        </form>
-                    </td>
+    
+    <div class="table-responsive">
+        
+        <table  class="table table-striped table-bordered table-sm"
+        >
+            <thead>
+                <tr class="">
+                    <th scope="col">#</th>
+                    <th scope="col">Title</th>
+                    <th scope="col">Description</th>
+                    <th scope="col">Difficulty</th>
+                    <th scope="col">Edit</th>
+                    <th scope="col">Delete</th>
                 </tr>
-            @endforeach
-        </tbody>
-    </table>
+            </thead>
+        
+                <tbody >
+                    @foreach ($projects as $project)
+                        <tr>
+                            <th scope="row">{{ $project->id }}</th>
+                            <td><a href="{{ route('admin.projects.show', $project->slug) }}"
+                                    title="View project">{{ $project->name }}</a></td>
+                            <td>{{ Str::limit($project->description, 100) }}</td>
+                            <td class="text-center">{{$project->diff_lvl}}</td>
+                            <td class="text-center"><a class="link-secondary" href="{{ route('admin.projects.edit', $project->slug) }}"
+                                    title="Edit project"><i class="fa-solid fa-pen"></i></a></td>
+                            <td >
+                                <form action="{{ route('admin.projects.destroy', $project->slug) }}" method="POST">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="delete-button btn btn-danger ms-3 "
+                                        data-item-title="{{ $project->name }}"><i class="fa-solid fa-trash-can"></i></button>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+        
+        </table>
+    </div>
     @include('partials.admin.modal-delete')
 @endsection
+
+
+
